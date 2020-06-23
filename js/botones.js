6b4117e5cb0cffcecad4447c0f4e484cdd95f6f8
Juan Arias <arias.juan.jose@outlook.com>
2020-06-23 13:01:59 -0300

    Se agregaron botones y responsive

---
+++ js/botones.js
@@ -0,0 +1,23 @@
+function cambioColorVerde(){
+    let color2 = document.getElementById('colorB');
+    let color3 = document.getElementById('colorC');
+
+    color2.className = "fechaVerde green d-flex justify-content-around m-5 reloj flex-wrap"
+    color3.className = "green mb-5 text-center"
+}
+
+function cambioColorAzul(){
+    let color2 = document.getElementById('colorB');
+    let color3 = document.getElementById('colorC');
+
+    color2.className = "fechaAzul blue  d-flex justify-content-around m-5 reloj flex-wrap"
+    color3.className = "blue mb-5 text-center"
+}
+
+function cambioColorRojo(){
+    let color2 = document.getElementById('colorB');
+    let color3 = document.getElementById('colorC');
+
+    color2.className = "fechaRojo pink d-flex justify-content-around m-5 reloj flex-wrap"
+    color3.className = "pink mb-5 text-center"
+}
